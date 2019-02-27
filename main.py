9ac795b4be48baab9172896318577f89aa26b5e6
lrcstars <lrcstars@qq.com>
2019-02-27 17:33:42 +0800

    save

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import  Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    config_ns = request.args.get('config_ns')
+    config_mode = request.args.get('config_mode')
+    data = request.get_json()
+    print config_ns
+    print congig_model
+    print data
     return '<h1>Hello World!</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
