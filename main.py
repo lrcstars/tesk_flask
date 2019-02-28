be18228f264dc1de8bb9605a533c4b8829137140
liruochen <liruochen@wealink.com>
2019-02-28 17:22:36 +0800

    save

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-from flask import  Flask, request
-app = Flask(__name__)
 with open("./OK.log", 'w') as fd:
     fd.write("OK")
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    config_ns = request.args.get('config_ns')
-    config_mode = request.args.get('config_mode')
-    data = request.get_json()
-    print config_ns
-    print config_mode
-    print data
-    return '<h1>Hello World!</h1>'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
